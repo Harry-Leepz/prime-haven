b89fc765c5452331ec4e1f9e795c1713faa141ab
Harry Dhillon <hdhillon478@gmail.com>
2024-10-31 13:05:59 +0000

    fix: fix typo in file and function name

---
+++ src/components/home/InfoBoxContainer.jsx
@@ -0,0 +1,3 @@
+export default function InfoBoxContainer() {
+  return <div>InfoBoxContianer</div>;
+}
--- src/components/home/InfoBoxContianer.jsx
+++
@@ -1,3 +0,0 @@
-export default function InfoBoxContianer() {
-  return <div>InfoBoxContianer</div>;
-}
