b437aa61d4b003a98b5d1f622750d1d65f097b06
Harry Dhillon <hdhillon478@gmail.com>
2024-10-31 13:53:30 +0000

    feat: implement InfoBox and InfoBoxContainer components

--- src/components/home/InfoBox.jsx
+++ src/components/home/InfoBox.jsx
@@ -1,3 +1,22 @@
-export default function InfoBox() {
-  return <div>InfoBox</div>;
+import Link from "next/link";
+
+export default function InfoBox({
+  title,
+  description,
+  linkText,
+  linkUrl,
+  backgroundColor,
+}) {
+  return (
+    <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+      <h2 className='text-2xl font-bold'>{title}</h2>
+      <p className='mt-2 mb-4'>{description}</p>
+      <Link
+        href={linkUrl}
+        className={`inline-block bg-slate-900 text-white rounded-lg px-4 py-2 hover:bg-gray-500`}
+      >
+        {linkText}
+      </Link>
+    </div>
+  );
 }
