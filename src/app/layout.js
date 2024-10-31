c7139f9c1bcd1a16bea5b0ff591e2318b16bc920
Harry Dhillon <hdhillon478@gmail.com>
2024-10-31 13:25:18 +0000

    feat: integrate Footer component into RootLayout for improved page structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,13 @@
+import { Inter } from "next/font/google";
+
 import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: "Prime Haven",
@@ -9,11 +17,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={inter.className}>
       <body>
         <main>
           <Navbar />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
