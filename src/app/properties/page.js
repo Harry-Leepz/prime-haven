8ec8c39fe6b6796207f3c9c12aa8865252ca4553
Harry Dhillon <hdhillon478@gmail.com>
2024-10-31 21:01:50 +0000

    feat: implement properties display with PropertyCard component

--- src/app/properties/page.js
+++ src/app/properties/page.js
@@ -1,3 +1,20 @@
+import PropertyCard from "@/components/layout/PropertyCard";
+import propertiesData from "@/fixtures/properties.json";
+
 export default function PropertiesPage() {
-  return <div>This is the Properties Page</div>;
+  console.log(propertiesData);
+  return (
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {propertiesData.length === 0 && <p>No properties found</p>}
+        {propertiesData.length > 0 && (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {propertiesData.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
