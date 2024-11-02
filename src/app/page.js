5fb4696691c66b859598f5f9f1db187d6115ad21
Harry Dhillon <hdhillon478@gmail.com>
2024-11-02 14:33:42 +0000

    feat: add FeaturedPropertiesContainer to HomePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/home/Hero";
 import InfoBoxContianer from "@/components/home/InfoBoxContainer";
+import FeaturedPropertiesContainer from "@/components/home/FeaturedPropertiesContainer";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <InfoBoxContianer />
+      <FeaturedPropertiesContainer />
     </>
   );
 }
