994910207ca875a698444d2d7645d6b2df5bae80
Harry Dhillon <hdhillon478@gmail.com>
2024-11-04 21:08:59 +0000

    feat: add User model with schema for user data and bookmarks

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: [true, "Email already exists"],
+    },
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Properties",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
