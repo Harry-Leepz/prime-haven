4ed1184a3b76780ae02f1eab09f54a31d95faa51
Harry Dhillon <hdhillon478@gmail.com>
2024-11-05 11:34:40 +0000

    refactor: integrate MongoDB to fetch properties dynamically in PropertiesPage

--- src/app/properties/page.js
+++ src/app/properties/page.js
@@ -1,14 +1,19 @@
-import PropertyCard from "@/components/layout/PropertyCard";
-import propertiesData from "@/fixtures/properties.json";
+import connectDB from "../../../config/database";
+import Property from "../../../models/Property";
 
-export default function PropertiesPage() {
+import PropertyCard from "@/components/layout/PropertyCard";
+
+export default async function PropertiesPage() {
+  await connectDB();
+  const properties = await Property.find({}).lean();
+
   return (
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
-        {propertiesData.length === 0 && <p>No properties found</p>}
-        {propertiesData.length > 0 && (
+        {properties.length === 0 && <p>No properties found</p>}
+        {properties.length > 0 && (
           <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {propertiesData.map((property) => (
+            {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
           </div>
