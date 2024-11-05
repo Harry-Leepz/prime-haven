9a74f92b6e03c14f6348f780d583b39bcbdb3d0d
Harry Dhillon <hdhillon478@gmail.com>
2024-11-05 13:43:12 +0000

    feat: implement PropertyDetailsPage to display property details with header image and navigation

--- src/app/properties/[id]/page.js
+++ src/app/properties/[id]/page.js
@@ -1,3 +1,39 @@
-export default function PropertyDetailsPage() {
-  return <div>This is the Property Details Page</div>;
+import Link from "next/link";
+
+import { FaArrowLeft } from "react-icons/fa";
+
+import connectDB from "../../../../config/database";
+import Property from "../../../../models/Property";
+
+import PropertyHeaderImage from "@/components/propertyDetails/PropertyHeaderImage";
+
+export default async function PropertyDetailsPage({ params }) {
+  await connectDB();
+  const property = await Property.findById(params.id).lean();
+
+  console.log(property);
+
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} alt={property.name} />
+
+      <section className='max-w-sm my-6 px-6'>
+        <Link
+          href={"/properties"}
+          className='bg-slate-900 text-white font-bold py-4 px-6 rounded-xl hover:bg-slate-500 text-center flex items-center'
+        >
+          <FaArrowLeft className='mr-2' />
+          View All Properties
+        </Link>
+      </section>
+
+      <section className='bg-blue-100'>
+        <div className='container m-auto py-10 px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            {/* Property Info section goes here */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
