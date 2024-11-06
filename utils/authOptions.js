a5a0c5d4e3525578474607814f7b107129351945
Harry Dhillon <hdhillon478@gmail.com>
2024-11-06 16:22:41 +0000

    feat: add authentication options with Google provider for next-auth

---
+++ utils/authOptions.js
@@ -0,0 +1,38 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          access_type: "offline",
+          prompt: "consent",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    // invoked on successful sign in
+    async signIn({ profile }) {
+      /*
+        TODO: Implement signIn callback
+          - Connect to the database
+          - Check if the user already exists
+          - If the user does not exist, create a new user
+          - Return true to allow sign in
+      */
+    },
+    // Session callback function that modifies the session object
+    async session({ session, token }) {
+      /*
+      TODO: Implement session callback
+        - Get user from databse
+        - Assign use id from  the session
+        - Return the session object
+      */
+    },
+  },
+};
