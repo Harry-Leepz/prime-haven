35a62340b0dbd9adee43d8e100a9119465a309e0
Harry Dhillon <hdhillon478@gmail.com>
2024-11-08 20:21:16 +0000

    feat: integrate AddPropertyForm into AddPropertyPage

--- src/app/properties/add/page.js
+++ src/app/properties/add/page.js
@@ -1,3 +1,13 @@
+import AddPropertyForm from "@/components/addProperty/AddPropertyForm";
+
 export default function AddPropertyPage() {
-  return <div>Add new Property Page</div>;
+  return (
+    <section className='bg-slate-100'>
+      <div className='container m-auto max-w-2xl py-24'>
+        <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
+          <AddPropertyForm />
+        </div>
+      </div>
+    </section>
+  );
 }
