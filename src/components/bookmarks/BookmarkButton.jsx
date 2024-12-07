ca3049effa6fc3fa6dbf84d5db2a29a301bc2790
Harry Dhillon <hdhillon478@gmail.com>
2024-12-07 20:37:36 +0000

    style: enhance BookmarkButton with styling and icon integration

--- src/components/bookmarks/BookmarkButton.jsx
+++ src/components/bookmarks/BookmarkButton.jsx
@@ -1,3 +1,9 @@
-export default function BookmarkButton() {
-  return <div>BookmarkButton</div>;
+import { FaBookmark } from "react-icons/fa";
+
+export default function BookmarkButton({ property }) {
+  return (
+    <button className='bg-slate-900 hover:bg-slate-950 text-white font-bold w-full py-4 rounded-full flex items-center justify-center gap-3'>
+      <FaBookmark /> Bookmark Property
+    </button>
+  );
 }
