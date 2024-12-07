8ea466284fd723268780fc2bde6913d81e690e14
Harry Dhillon <hdhillon478@gmail.com>
2024-12-07 20:40:08 +0000

    style: enhance SocialShareButtons with styling

--- src/components/social-share/SocialShareButtons.jsx
+++ src/components/social-share/SocialShareButtons.jsx
@@ -1,3 +1,9 @@
-export default function SocialShareButtons() {
-  return <div>SocialShareButtons</div>;
+import { FaShare } from "react-icons/fa";
+
+export default function SocialShareButtons({ property }) {
+  return (
+    <button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-4 rounded-full flex items-center justify-center gap-3'>
+      <FaShare /> Share Property
+    </button>
+  );
 }
