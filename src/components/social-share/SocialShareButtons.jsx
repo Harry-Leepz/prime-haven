389fbf7c256c1896e732ba6b012250c8c0277ba1
Harry Dhillon <hdhillon478@gmail.com>
2024-12-22 19:55:28 +0000

    feat: implement social sharing buttons for property sharing using react-share

--- src/components/social-share/SocialShareButtons.jsx
+++ src/components/social-share/SocialShareButtons.jsx
@@ -1,9 +1,45 @@
-import { FaShare } from "react-icons/fa";
+"use client";
+
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+} from "react-share";
 
 export default function SocialShareButtons({ property }) {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
+
   return (
-    <button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-4 rounded-full flex items-center justify-center gap-3'>
-      <FaShare /> Share Property
-    </button>
+    <>
+      <h3 className='text-xl font-bold text-center pt-2'>
+        Share this property:
+      </h3>
+      <div className='flex gap-3 justify-center pb-5'>
+        <FacebookShareButton
+          url={shareUrl}
+          quote={property.name}
+          hashtag={`#${property.type.replace(/\s/g, "")}ForRent`}
+        >
+          <FacebookIcon size={40} round />
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={shareUrl}
+          title={property.name}
+          hashtags={[`#${property.type.replace(/\s/g, "")}ForRent`]}
+        >
+          <TwitterIcon size={40} round />
+        </TwitterShareButton>
+        <WhatsappShareButton
+          url={shareUrl}
+          title={property.name}
+          separator=' :: '
+        >
+          <WhatsappIcon size={40} round />
+        </WhatsappShareButton>
+      </div>
+    </>
   );
 }
