92ac34438a4a6e3887d31bf62049d37ea6fc5c87
Harry Dhillon <hdhillon478@gmail.com>
2024-12-25 20:00:23 +0000

    feat: implement property search functionality in SearchResultsPage

--- src/app/properties/search-results/page.jsx
+++ src/app/properties/search-results/page.jsx
@@ -1,3 +1,36 @@
-export default function SearchResultsPage() {
+import connectDB from "../../../../config/database";
+
+import Property from "../../../../models/Property";
+
+import convertToObject from "@/utils/convertToObject";
+
+export default async function SearchResultsPage({
+  searchParams: { location, type },
+}) {
+  console.log(location, type);
+  await connectDB();
+
+  const locationRegex = new RegExp(location, "i");
+
+  let query = {
+    $or: [
+      { name: locationRegex },
+      { description: locationRegex },
+      { "location.street": locationRegex },
+      { "location.city": locationRegex },
+      { "location.state": locationRegex },
+      { "location.zipcode": locationRegex },
+    ],
+  };
+
+  if (type && type !== "All") {
+    const typeRegex = new RegExp(type, "i");
+    query.type = typeRegex;
+  }
+
+  const propertiesQueryResults = await Property.find(query).lean();
+  const properties = convertToObject(propertiesQueryResults);
+  console.log(properties);
+
   return <div>page</div>;
 }
