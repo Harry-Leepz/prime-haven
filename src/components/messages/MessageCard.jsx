ef8c568706932a05acbcd6b5ea26d1abf3704ccc
Harry Dhillon <hdhillon478@gmail.com>
2024-12-30 16:49:46 +0000

    feat: enhance MessageCard to toggle read status with user feedback

--- src/components/messages/MessageCard.jsx
+++ src/components/messages/MessageCard.jsx
@@ -1,6 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
+import { toast } from "react-toastify";
+
+import markMessageAsRead from "@/app/actions/markMessageAsRead";
+
 export default function MessageCard({ message }) {
+  const [isRead, setIsRead] = useState(message.read);
+
+  const handleReadClick = async () => {
+    const read = await markMessageAsRead(message._id);
+    setIsRead(read);
+    toast.success(`Message marked as ${read ? "read" : "unread"}`);
+  };
+
   return (
     <div className='relative bg-white p-4 rounded-md shadow-md border border-slate-200'>
+      {!isRead && (
+        <div className='absolute top-2 right-2 bg-yellow-500 text-white py-1 px-2 rounded-tl-md rounded-br-md'>
+          New
+        </div>
+      )}
       <h2 className='text-xl mb-4'>
         <span className='font-bold'>Property Inquiry: </span>
         {message.property.name}
@@ -26,8 +47,11 @@ export default function MessageCard({ message }) {
         </li>
       </ul>
 
-      <button className='mt-4 mr-3 bg-slate-900 text-white py-1 px-3 rounded-md'>
-        Mark As read
+      <button
+        className='mt-4 mr-3 bg-slate-900 text-white py-1 px-3 rounded-md'
+        onClick={handleReadClick}
+      >
+        {isRead ? "Mark as Unread" : "Mark as Read"}
       </button>
       <button className='mt-4 bg-red-500 text-white py-1 px-3 rounded-md'>
         Delete
