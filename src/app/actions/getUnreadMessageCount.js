28bff6e6841a4e0d37d470762ee248c5f85fda33
Harry Dhillon <hdhillon478@gmail.com>
2024-12-31 17:13:48 +0000

    feat: add getUnreadMessageCount action to retrieve unread message count for the user

---
+++ src/app/actions/getUnreadMessageCount.js
@@ -0,0 +1,28 @@
+"use server";
+
+import connectDB from "../../../config/database";
+
+import Message from "../../../models/Message";
+
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export default async function getUnreadMessageCount() {
+  await connectDB();
+
+  const sessionsUser = await getSessionUser();
+
+  if (!sessionsUser || !sessionsUser.userId) {
+    throw new Error("User ID is required");
+  }
+
+  const { userId } = sessionsUser;
+
+  const count = await Message.countDocuments({
+    recipient: userId,
+    read: false,
+  });
+
+  console.log("count", count);
+
+  return count;
+}
