d0f9a9c0c75601740cc883c31524e0d7aed9876e
Harry Dhillon <hdhillon478@gmail.com>
2024-12-31 17:13:55 +0000

    feat: fetch unread message count on session change in MessageProvider

--- src/context/MessageContext.js
+++ src/context/MessageContext.js
@@ -1,11 +1,26 @@
 "use client";
 
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
+
+import { useSession } from "next-auth/react";
+
+import getUnreadMessageCount from "@/app/actions/getUnreadMessageCount";
 
 const MessageContext = createContext();
 
 export function MessageProvider({ children }) {
   const [unreadMessageCount, setUnreadMessageCount] = useState(0);
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session && session.user) {
+      getUnreadMessageCount(session.userId).then((count) => {
+        console.log("count", count);
+        setUnreadMessageCount(count);
+      });
+    }
+  }, [session]);
 
   return (
     <MessageContext.Provider
