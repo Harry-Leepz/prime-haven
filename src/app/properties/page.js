7bc15dc3345e1f866daef7f7a2d928656d172fab
Harry Dhillon <hdhillon478@gmail.com>
2025-01-04 18:45:54 +0000

    feat: enhance properties page with pagination controls and increase page size

--- src/app/properties/page.js
+++ src/app/properties/page.js
@@ -1,21 +1,24 @@
+import PaginationControls from "@/components/pagination/PaginationControls";
 import connectDB from "../../../config/database";
 import Property from "../../../models/Property";
 
 import PropertyCard from "@/components/layout/PropertyCard";
 
 export default async function PropertiesPage({
-  searchParams: { page = 1, pageSize = 2 },
+  searchParams: { page = 1, pageSize = 6 },
 }) {
   await connectDB();
 
   // pagination logic
   const skip = (page - 1) * pageSize;
-  const total = Property.countDocuments({});
+  const total = await Property.countDocuments({});
 
   const properties = await Property.find({}).skip(skip).limit(pageSize);
 
+  const showPagination = total > pageSize;
+
   return (
-    <section className='px-4 py-6'>
+    <section className='px-4 py-6 min-h-[81vh]'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
         {properties.length === 0 && <p>No properties found</p>}
         {properties.length > 0 && (
@@ -26,6 +29,13 @@ export default async function PropertiesPage({
           </div>
         )}
       </div>
+      {showPagination && (
+        <PaginationControls
+          page={parseInt(page)}
+          pageSize={parseInt(pageSize)}
+          total={parseInt(total)}
+        />
+      )}
     </section>
   );
 }
